bc52ec2fe19abcfadf1514101c7e1ebe416e3fc6
K-yoon03 <yoonnyang1013@gmail.com>
2026-07-06 16:50:36 +0900

    Init: TTD Backend SpringBoot Project Initializing

---
+++ src/main/java/kr/ac/kopo/ttd/TtdApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.ttd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtdApplication.class, args);
+	}
+
+}
