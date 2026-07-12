4269642b224a7aeafc68424f9d466c94908872b8
chayh414 <chayh414@gmail.com>
2026-07-12 19:15:14 +0900

    [SCRUM-74] feat: 닉네임 변경 API 추가

--- src/main/java/kr/ac/kopo/ttd/service/UserProfileService.java
+++ src/main/java/kr/ac/kopo/ttd/service/UserProfileService.java
@@ -3,9 +3,11 @@ package kr.ac.kopo.ttd.service;
 import kr.ac.kopo.ttd.common.exception.UserNotFoundException;
 import kr.ac.kopo.ttd.domain.User;
 import kr.ac.kopo.ttd.dto.MyProfileResponse;
+import kr.ac.kopo.ttd.dto.NicknameUpdateRequest;
 import kr.ac.kopo.ttd.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +18,20 @@ public class UserProfileService {
     public MyProfileResponse getMyProfile(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(UserNotFoundException::new);
+        return new MyProfileResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getNickname(),
+                user.getRole().toString(),
+                user.getCreatedAt()
+        );
+    }
+
+    @Transactional
+    public MyProfileResponse changeNickname(Long userId, NicknameUpdateRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+        user.changeNickname(request.nickname());
         return new MyProfileResponse(
                 user.getId(),
                 user.getEmail(),
