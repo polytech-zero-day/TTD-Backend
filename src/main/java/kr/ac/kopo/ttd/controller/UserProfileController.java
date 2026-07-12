9d3707edd8f4dfe4260a8d05382dc3a9670255f3
chayh414 <chayh414@gmail.com>
2026-07-12 19:15:14 +0900

    [SCRUM-74] feat: 닉네임 변경 API 추가

--- src/main/java/kr/ac/kopo/ttd/controller/UserProfileController.java
+++ src/main/java/kr/ac/kopo/ttd/controller/UserProfileController.java
@@ -1,12 +1,16 @@
 package kr.ac.kopo.ttd.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import kr.ac.kopo.ttd.common.ApiResponse;
 import kr.ac.kopo.ttd.dto.MyProfileResponse;
+import kr.ac.kopo.ttd.dto.NicknameUpdateRequest;
 import kr.ac.kopo.ttd.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,12 @@ public class UserProfileController {
     @GetMapping
     public ApiResponse<MyProfileResponse> getMyProfile(@AuthenticationPrincipal Long userId) {
         return ApiResponse.success(userProfileService.getMyProfile(userId));
+    }
+
+    @PatchMapping("/nickname")
+    public ApiResponse<MyProfileResponse> changeNickname(
+            @AuthenticationPrincipal Long userId,
+            @Valid @RequestBody NicknameUpdateRequest request) {
+        return ApiResponse.success(userProfileService.changeNickname(userId, request));
     }
 }
